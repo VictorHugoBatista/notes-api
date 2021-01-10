cc72a31b8b2e53baa948547803a6c8f9a67531c8
Victor Hugo Batista <vct.bta@gmail.com>
2021-01-10 19:17:25 -0300

    Update and delete post on PATCH | DELETE /notes/:noteId endpoint

--- routes/notes.js
+++ routes/notes.js
@@ -26,21 +26,32 @@ router.post('/', async (req, res) => {
   const note = new Note();
   note.title = req.body.title;
   note.body = req.body.body;
-  note.save();
+  const result = await note.save();
+  res.send(result);
 });
 
 /**
  * Edit note by id.
  */
-router.patch('/:noteId', (req, res) => {
-  res.send(req.body);
+router.patch('/:noteId', async (req, res) => {
+  const result = await Note.updateOne(
+    {_id: req.params.noteId},
+    {
+      $set: {
+        title: req.body.title,
+        body: req.body.body,
+      },
+    }
+  );
+  res.send(result);
 });
 
 /**
  * Remove note by id.
  */
-router.delete('/:noteId', (req, res) => {
-  res.send(req.params.noteId);
+router.delete('/:noteId', async (req, res) => {
+  const result = await Note.deleteOne({_id: req.params.noteId});
+  res.send(result);
 });
 
 module.exports = router;
