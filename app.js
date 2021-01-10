72749a7ae51ab94b1e13f6613fa4534d69a35a48
Victor Hugo Batista <vct.bta@gmail.com>
2021-01-10 18:01:49 -0300

    Remove the static resources from project

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var notesRouter = require('./routes/notes');
 
 var app = express();
@@ -12,9 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/notes', notesRouter);
 
 module.exports = app;
