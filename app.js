cc5950d7175ed859f266c67b28691da26a386973
Victor Hugo Batista <vct.bta@gmail.com>
2021-01-10 18:18:21 -0300

    Load dotenv .env file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('dotenv').config();
 
 const notesRouter = require('./routes/notes');
 
